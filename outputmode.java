8c92727839e929719752d22c06e8f1db474a7b8d
HalfBlu3 <73621901+HalfBlu3@users.noreply.github.com>
2023-01-20 07:09:04 -0800

    Update outputmode.java

--- outputmode.java
+++ outputmode.java
@@ -4,11 +4,13 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.TouchSensor;
 
 @TeleOp(name = "outputmode", group = "whatever")
 public class outputmode extends LinearOpMode {
     DcMotor FLeft, FRight, BLeft, BRight, UpperArm, Turret, LowerArm;
-    Servo Claw;
+    Servo Claw, Wrist;
+    TouchSensor ButtonOne, ButtonTwo;
 
     @Override
     public void runOpMode() {
@@ -20,19 +22,34 @@ public class outputmode extends LinearOpMode {
         Turret = hardwareMap.get(DcMotor.class, "Turret");
         UpperArm = hardwareMap.get(DcMotor.class, "UpperArm");
         Claw = hardwareMap.get(Servo.class, "Claw");
-        double max;
-        double sri=0;
-        waitForStart();
-        while (opModeIsActive() && (!x() && !gamepad2.x)) {
-            //gamepad1
-            double pa = leftStickX();
-            double px = Math.round(-leftStickY()/2);
-            double py = -rightStickX();
+        Wrist = hardwareMap.get(Servo.class, "Wrist");
+        ButtonOne = hardwareMap.touchSensor.get("ButtonOne");
+        ButtonTwo = hardwareMap.touchSensor.get("ButtonTwo");
 
+        double max;
+        final int UpperPos = UpperArm.getCurrentPosition();
+        final int LowerPos = LowerArm.getCurrentPosition();
+        final int TurretPos = Turret.getCurrentPosition();
+        final double WristPos = Wrist.getPosition();
+
+        //target positions for arm presets
+        final int UpperArmDown = UpperPos - 2091;
+        final int LowerArmDown = LowerPos + 8522;
+        final int UpperArmUp = 0; //TODO
+        final int LowerArmUp = 0; //TODO
+        int mode = 0;
+
+        waitForStart();
+
+        while (opModeIsActive() && (!(gamepad1.x || gamepad2.x))) {
+            //mechanum math stuff
+            double py = -gamepad1.right_stick_x;
+            double px = Math.round(gamepad1.left_stick_x);
+            double pa = -gamepad1.left_stick_y;
             if (Math.abs(pa) < 0.05) pa = 0;
-            double pFLeft = -px + py -pa;
-            double pFRight = px + py -pa;
-            double pBLeft = -px + py + pa;
+            double pFLeft = px + py - pa;
+            double pFRight = -px + py + pa;
+            double pBLeft = -px + py - pa;
             double pBRight = px + py + pa;
             max = Math.max(1.0, Math.max(Math.abs(pFLeft), Math.max(Math.abs(pFRight), Math.max(Math.abs(pBLeft), Math.abs(pBRight)))));
             pFLeft /= max;
@@ -44,45 +61,41 @@ public class outputmode extends LinearOpMode {
             BLeft.setPower(pBLeft);
             BRight.setPower(pBRight);
 
-            //gamepad2
-            LowerArm.setPower(gamepad2.left_stick_y);
-            UpperArm.setPower(gamepad2.right_stick_y);
-            Turret.setPower((gamepad2.left_trigger > 0) ? 0.5 : (gamepad2.right_trigger > 0) ? -0.5 : 0);
-            Claw.setPosition((gamepad2.right_bumper || gamepad2.left_bumper) ? 1 : 0.2);
+            //turret and arm controls
+            //sets turret to how far the trigger is pressed, but all the way is too fast. I might add a hard limit and have a separate speed button
+            Turret.setPower((gamepad2.left_trigger > 0) ? gamepad2.left_trigger : (gamepad2.right_trigger > 0) ? -gamepad2.right_trigger : 0);
+            Claw.setPosition(gamepad2.right_bumper ? 0.5 : -0.1);
+            Wrist.setPosition((gamepad2.left_bumper) ? 0 : 1); //wrist doesn't currently work
 
-            //print stuff or something
-            if (a() || gamepad2.a) {
-                telemetry.addData("Upper Arm", UpperArm.getCurrentPosition());
-                telemetry.addData("Lower Arm", LowerArm.getCurrentPosition());
+            //squished all three modes into some ternaries to compact the code
+            //currently you cannot manually control motors while preset is being enacted. this limit is for testing and will be removed later
+            UpperArm.setPower(mode == 0 ? gamepad2.right_stick_y : mode == 1 && UpperArm.getCurrentPosition() > UpperArmDown ? -1 : mode == 2 && UpperArm.getCurrentPosition() < UpperArmUp ? 1 : 0);
+            LowerArm.setPower(mode == 0 ? gamepad2.right_stick_y : mode == 1 && LowerArm.getCurrentPosition() < LowerArmDown ? 1 : mode == 2 && LowerArm.getCurrentPosition() > LowerArmUp ? -1 : 0);
+            mode = (mode == 0 && gamepad2.dpad_down ? 1 : mode == 0 && gamepad2.dpad_up ? 2 : (mode == 1 && UpperArm.getCurrentPosition() <= UpperArmDown && LowerArm.getCurrentPosition() >= LowerArmDown) || (mode == 2 && UpperArm.getCurrentPosition() >= UpperArmUp && LowerArm.getCurrentPosition() <= LowerArmUp) ? 0 : mode);
+
+            //print encoder info
+            if (gamepad1.a || gamepad2.a) {
+                telemetry.addData("Upper Arm start", UpperPos);
+                telemetry.addData("Upper Arm current", UpperArm.getCurrentPosition());
+                telemetry.addData("Lower Arm start", LowerPos);
+                telemetry.addData("Lower Arm current", LowerArm.getCurrentPosition());
                 telemetry.addData("Turret", Turret.getCurrentPosition());
+                telemetry.addData("ButtonOne", ButtonOne.isPressed());
+                telemetry.addData("ButtonTwo", ButtonTwo.isPressed());
+                telemetry.addData("Claw" , Claw.getPosition());
                 telemetry.update();
             }
 
-            if (y() || gamepad2.y){
-                UpperArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                LowerArm.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-                UpperArm.setTargetPosition(0);
-                LowerArm.setTargetPosition(0);
-                UpperArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-                LowerArm.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-
+            //arm reset
+            if (gamepad1.y || gamepad2.y){
+                //do upper arm first and then lower arm
+                //I might eventually be able to condense this to one ternary in a WHILE loop, I just haven't gotten around to it
+                while (UpperArm.getCurrentPosition() != UpperPos) UpperArm.setPower(UpperArm.getCurrentPosition() > UpperPos ? -0.7 : 0.7);
+                UpperArm.setPower(0);
+                while (LowerArm.getCurrentPosition() != LowerPos) LowerArm.setPower(LowerArm.getCurrentPosition() > LowerPos ? -0.7 : 0.7);
+                LowerArm.setPower(0);
+                Wrist.setPosition(WristPos); //wrist doesn't actually work
             }
         }
     }
-    public boolean RTrigger(){return(gamepad1.right_trigger != 0);}
-    public boolean LTrigger(){return(gamepad1.left_trigger != 0);}
-    public boolean a(){return gamepad1.a;}
-    public boolean b(){return gamepad1.b;}
-    public boolean x(){return gamepad1.x;}
-    public boolean y(){return gamepad1.y;}
-    public boolean dUp(){return gamepad1.dpad_up;}
-    public boolean dDown(){return gamepad1.dpad_down;}
-    public boolean dLeft(){return gamepad1.dpad_left;}
-    public boolean dRight(){return gamepad1.dpad_right;}
-    public boolean rBumper(){return gamepad1.right_bumper;}
-    public boolean lBumper(){return gamepad1.left_bumper;}
-    public double leftStickX(){return gamepad1.left_stick_x;}
-    public double leftStickY(){return gamepad1.left_stick_y;}
-    public double rightStickX(){return gamepad1.right_stick_x;}
-    public double rightStickY(){return gamepad1.right_stick_y;}
 }
